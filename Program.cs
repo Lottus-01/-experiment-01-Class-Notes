dd860ffbdb2af33c603cc38405670a35ceb75313
Vittoria PL <Lopes.tt06@gmail.com>
2025-03-21 22:27:06 -0300

    test concluido

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+double numero1, numero2;
+double resultado;
+
+Console.WriteLine("insira numero aleatorio:");
+    numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("insira numero aleatorio:");
+    numero2 = Convert.ToInt32(Console.ReadLine());
+
+
+resultado = numero1 * numero2;
+
+Console.WriteLine();
+
+Console.WriteLine("Resultado:");
+    Console.WriteLine($"{numero1} * {numero2} = {resultado:N5}");
+
